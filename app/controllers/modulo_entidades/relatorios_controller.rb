9fd054653542b2db2b385896d8e3c1bd920a6ab4
Paulo Zeferino <paulo@devconnit.com>
2014-02-07 09:10:31 -0200

    Enviando Relatório de Pesquisas Consolidadas

--- app/controllers/modulo_entidades/relatorios_controller.rb
+++ app/controllers/modulo_entidades/relatorios_controller.rb
@@ -5,6 +5,16 @@ class ModuloEntidades::RelatoriosController < ApplicationController
 	def pesquisas_respondidas
 		params[:pesquisa] ||= {}
 		@auditorias = Relatorio.pesquisas_respondidas(current_unidade.id, params[:pesquisa])
+		@numero_de_conformidades = 0
+		@numero_de_nao_conformidades = 0
+		@auditorias.each do |auditoria|
+			p "daiuhsdfiuahsdfuihsdaui 123984723984758934"
+			@numero_de_conformidades += auditoria.numero_de_conformidades
+			@numero_de_nao_conformidades += auditoria.numero_de_nao_conformidades
+		end
+		p "siudhfaiusdhfiuasd"
+		p @numero_de_conformidades
+		p @numero_de_nao_conformidades
 	end
 
 	def relatorio_acoes
