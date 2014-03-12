f18f1916cdb2aa7e3e5ca0d9e8626745a4eb2a6e
Guilherme <guilherme@devconnit.com>
2014-03-12 16:12:44 -0300

    Mudando o numerador

--- app/models/erp_ger_numerador.rb
+++ app/models/erp_ger_numerador.rb
@@ -15,14 +15,18 @@ class ErpGerNumerador < ConexaoPanambra
   	## Devera entrar na tabela GER_NUMERADOR, buscar o campo próximo número da empresa e revenda
   	## relacionada a nota e imediatamente somar +1
   	configuracao  = Configuracao.first
-  	ultimo_numero = ErpGerNumerador.where(empresa: configuracao.empresa)
+  	ultimo_numero = 0
+    numerador     = ErpGerNumerador.where(empresa: configuracao.empresa)
                                    .where(revenda: configuracao.revenda)
   								 							   .where(tabela: tabela)
                                    .where(numerador: numerador)
                                    .order('proximo_numero DESC')
-                                   .first
-                                   .proximo_numero rescue 0
-		proximo_numero = ultimo_numero + 1
+    if numerador.present?
+      ultimo_numero = numerador.first.proximo_numero rescue 0
+      proximo_numero = ultimo_numero + 1
+      numerador.first.update_column(proximo_numero: proximo_numero)
+    end
+    ultimo_numero
   end
 
 end
