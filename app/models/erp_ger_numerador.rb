f23d21b79b637cfce472e4968183a4c62d79788a
Guilherme <guilherme@devconnit.com>
2014-03-12 16:52:06 -0300

    Numerador

--- app/models/erp_ger_numerador.rb
+++ app/models/erp_ger_numerador.rb
@@ -24,7 +24,10 @@ class ErpGerNumerador < ConexaoPanambra
     if numerador.present?
       ultimo_numero = numerador.first.proximo_numero rescue 0
       proximo_numero = ultimo_numero + 1
-      numerador.first.update_column(:proximo_numero, proximo_numero)
+      sql  = "UPDATE ERP_GER_NUMERADOR SET proximo_numero = #{proximo_numero} "
+      sql += "WHERE empresa = #{configuracao.empresa} AND revenda = #{configuracao.revenda} AND "
+      sql += "tabela = '#{tabela}' AND numerador = '#{numerador}'"
+      connection.execute(sql)
     end
     ultimo_numero
   end
