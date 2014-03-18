3c77f20b222b6ec9fab1f06ff6ec95f104883281
Guilherme <guilherme@devconnit.com>
2014-03-18 16:39:51 -0300

    xx"

--- app/models/gcm.rb
+++ app/models/gcm.rb
@@ -1,22 +1,23 @@
 #encoding: UTF-8
+
 class Gcm < ActiveRecord::Base
-  
 
 	def self.create_configuration
 		Push::ConfigurationGcm.create(
 			app: 'com.example.panambramobile', 
 			connections: 2, 
 			enabled: true,
-    		key: 'AIzaSyBNh0TGVFYbLC5w7XyOS3Nbbac3FvCYJx4')
+			key: 'AIzaSyBNh0TGVFYbLC5w7XyOS3Nbbac3FvCYJx4'
+		)
 	end
 
-
 	def self.send_android_message(user_gcm)
-	    Push::MessageGcm.create(
-		    app: 'com.example.panambramobile',
-    		device: user_gcm,
-    		payload: { message: 'Existe uma nova Ocorrência' },
-    		collapse_key: 'MSG')
+		Push::MessageGcm.create(
+			app: 'com.example.panambramobile',
+    	device: user_gcm,
+    	payload: { message: 'Existe uma nova Ocorrência' },
+    	collapse_key: 'MSG'
+		)
 	end
 
 end
