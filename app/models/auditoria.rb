cde9fda56abffb04db4371527d0689be55689f92
Panambra <totem1@panambra.com.br>
2014-05-31 15:46:23 -0300

    Enviando

--- app/models/auditoria.rb
+++ app/models/auditoria.rb
@@ -77,7 +77,7 @@ class Auditoria < ActiveRecord::Base
       auditoria.cliente   = cliente
       auditoria.unidade   = current_unidade
       auditoria.checklist = current_unidade.retorna_checklist_ativo
-      auditoria.numero_ordem = ordem 
+     # auditoria.numero_ordem = ordem 
       if auditoria.save
         [true, auditoria]
       else
