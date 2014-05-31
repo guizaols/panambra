bb208581a4e9e556a8566de78802accbd156abd6
Panambra <totem1@panambra.com.br>
2014-05-31 14:07:43 -0300

    Enviando

--- app/models/erp_ofi_atendimento.rb
+++ app/models/erp_ofi_atendimento.rb
@@ -49,9 +49,9 @@ class ErpOfiAtendimento < ConexaoPanambra
 			os.empresa=fmc.empresa AND
 			os.revenda=fmc.revenda AND
 			os.contato=fmc.contato AND
-			os.categoria_os=1 AND
-			fmc.departamento in(7,8) AND
-			fmc.tipo_transacao=21 AND
+			os.categoria_os='1' AND
+			fmc.departamento in('7','8') AND
+			fmc.tipo_transacao='O21' AND
 		    oa.empresa = #{configuracao.empresa} AND
             oa.revenda = #{configuracao.revenda} AND
 			fmc.dta_entrada_saida between '29/may/2014' and '29/may/2014'
